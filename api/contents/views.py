d2ffae1a37eb8d3045db9d886cdbaf15e4aba134
abdulsamedkeskin <erdemsametkeskin25@gmail.com>
2022-11-20 00:35:04 +0300

    add for_alert param for content request

--- api/contents/views.py
+++ api/contents/views.py
@@ -6,5 +6,12 @@ contents = Blueprint('contents', __name__, url_prefix='/contents')
 @contents.get("/<content_type>")
 def offers(content_type):
   language = request.args.get("language", "en-US")
+  for_alert = request.args.get("for_alert")
   contents = get_contents(language=language)
+  if for_alert:
+    data = []
+    for i in contents[0][content_type]:
+      if i['assetPath'].split("_PrimaryAsset")[0][-1] == "1":
+        data.append(i)
+    return data
   return contents[0][content_type]
